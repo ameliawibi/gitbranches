2ead9d621c119c80017c4b9848b0bb2e8e4331fe
tehkohrz <dongkunkoh@hotmail.com>
2022-01-29 11:26:42 +0800

    done edit 1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { writeFile } from 'fs';
 let name = process.argv[2];
+<<<<<<< HEAD
 writeFile('data.json', name, (err) => console.error('err ->', err));
 
 console.log(`good day ${name}, are you well DK?`);
+=======
+console.log(`hello ${name.toUpperCase()}! How are you?`);
+
+console.log('Chicken McSpicy');
+>>>>>>> 4b6ca55 (edit 1)
