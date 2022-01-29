990bcbcd10cad5e312b1514fb8bacf3c3d6cd39f
Amelia Wibisono <ameliawibi@gmail.com>
2022-01-29 11:31:21 +0800

    DK

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { writeFile } from 'fs';
 let name = process.argv[2];
 writeFile('data.json', name, (err) => console.error('err ->', err));
 
-console.log(`good day ${name}, are you well?`);
+console.log(`good day ${name}, are you well DK?`);
